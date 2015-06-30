5a2c02501924c4f2abbf30e7dae3b8b1d60f8ca5
Mark Steve Samson <hello@marksteve.com>
2015-06-30 11:41:55 +0800

    Initial commit

---
+++ client.py
@@ -0,0 +1,27 @@
+import datetime as dt
+import os
+import socket
+import time
+
+import picamera
+
+client_socket = socket.socket()
+client_socket.connect((os.environ['PICAMERA_SERVER'], 3141))
+
+connection = client_socket.makefile('wb')
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+        camera.start_preview()
+        time.sleep(2)
+        camera.annotate_background = picamera.Color('black')
+        camera.start_recording(connection, format='h264')
+        while True:
+            camera.annotate_text = (dt.datetime
+                                    .now()
+                                    .strftime('%Y-%m-%d %H:%M:%S'))
+            camera.wait_recording(0.2)
+finally:
+    connection.close()
+    client_socket.close()
---
+++ server.py
@@ -0,0 +1,35 @@
+# import os
+import socket
+import subprocess
+
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 3141))
+server_socket.listen(0)
+
+connection = server_socket.accept()[0].makefile('rb')
+try:
+    # Run a viewer with an appropriate command line. Uncomment the mplayer
+    # version if you would prefer to use mplayer instead of VLC
+    cmdline = ['vlc', '--demux', 'h264', '-']
+
+    cmdline = [
+        'vlc',
+        # '-I', 'http',
+        # '--http-host', '0.0.0.0',
+        # '--http-port', '8888',
+        # '--http-password', os.environ['VLC_HTTP_PASSWORD'],
+        '--sout', 'http/ts://0.0.0.0:31415',
+        '--demux', 'h264',
+        '-',
+    ]
+    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+    while True:
+        data = connection.read(1024)
+        if not data:
+            break
+        player.stdin.write(data)
+finally:
+    connection.close()
+    server_socket.close()
+    player.terminate()
+
